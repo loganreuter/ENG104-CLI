55bbef544181ba79c8c3f9b1dd8b61aacb44e56f
lreuter2020 <lreuter2020@gmail.com>
2022-07-28 22:31:03 -0700

    final commit before final

--- Evaluator/Functions/Plane_Strain.m
+++ Evaluator/Functions/Plane_Strain.m
@@ -6,6 +6,12 @@ function Plane_Strain()
     epsilon_x = input(sprintf("\tƐₓ: "));
     epsilon_y = input(sprintf("\tƐᵧ: "));
     
-    sigma_x = 
+    sigma_x = (youngs_modulus / ((1+poissons_ratio) * (1 - 2 * poissons_ratio))) * (((1 - poissons_ratio) * epsilon_x) + (poissons_ratio * epsilon_y));
+    sigma_y = (youngs_modulus / ((1+poissons_ratio) * (1 - 2 * poissons_ratio))) * (((1 - poissons_ratio) * epsilon_y) + (poissons_ratio * epsilon_x));
+    sigma_z = ((youngs_modulus * poissons_ratio) / ((1+poissons_ratio) * (1 - 2 * poissons_ratio))) * (epsilon_x + epsilon_y);
+
+    fprintf("σₓ: %f\n", sigma_x);
+    fprintf("σᵧ: %f\n", sigma_y);
+    fprintf("(Note: might be wrong) σ₂: %f\n\n", sigma_z); 
 end
 
